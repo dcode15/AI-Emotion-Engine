7ce43fe5003c849ac828a3a7f7818573c4229710
DCode <dcode10@gmail.com>
2015-02-22 17:44:54 -0500

    Added rule application to fuzzy logic library
    The applyRules function takes a set of rules of the format If X is A
    (AND/OR) Y is B...THEN Z is C and applies them to fuzzy input sets to
    get fuzzified values for output sets.

--- js/fuzzy/fuzzy.js
+++ js/fuzzy/fuzzy.js
@@ -19,7 +19,8 @@ fuzzySystem.prototype.processValue = function(value) {
 
     var fuzzyInputs = this.convertInputToFuzzy(value);
     console.log(fuzzyInputs);
-    //var fuzzyOutputs = this.applyRules(fuzzyInputs);
+    var fuzzyOutputs = this.applyRules(fuzzyInputs);
+    console.log(fuzzyOutputs);
 };
 
 fuzzySystem.prototype.convertInputToFuzzy = function(value) {
@@ -54,5 +55,52 @@ fuzzySystem.prototype.convertInputToFuzzy = function(value) {
 };
 
 fuzzySystem.prototype.applyRules = function(fuzzyInputs) {
-    return null;
+
+    var fuzzyOutputs = {};
+    for(var outputNum = 0; outputNum < this.outputSets.length; outputNum++) {
+        fuzzyOutputs[this.outputSets[outputNum].variableName] = {};
+        var sets = this.outputSets[outputNum].setValues;
+
+        for (var setNum = 0; setNum < sets.length; setNum++) {
+            var outputVar = this.outputSets[outputNum].variableName;
+            var setName = this.outputSets[outputNum].setNames[setNum];
+            fuzzyOutputs[outputVar][setName] = 0;
+        }
+    }
+
+    for(var ruleNum = 0; ruleNum < this.rules.length; ruleNum++) {
+        var operator = "IF";
+        var rule = this.rules[ruleNum];
+        var tokenNum = 0;
+        var total = 0;
+
+        while(operator !== "THEN") {
+            var varName = rule[tokenNum];
+            tokenNum++;
+            var setName = rule[tokenNum];
+            tokenNum++;
+
+            var value = fuzzyInputs[varName][setName];
+            if(operator === "IF") {
+                total = value;
+            }
+            if(operator === "AND") {
+                total = Math.max(value, total);
+            }
+            if(operator === "OR") {
+                total = Math.min(value, total);
+            }
+
+            operator = rule[tokenNum];
+            tokenNum++;
+        }
+
+        var outputVar = rule[tokenNum];
+        tokenNum++;
+        var outputSet = rule[tokenNum];
+        tokenNum++;
+        fuzzyOutputs[outputVar][outputSet] = total;
+    }
+
+    return fuzzyOutputs;
 }
--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@
 var input1 = new fuzzyVar("Temp", ["Cold", "Warm", "Hot"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.8,1]]);
 var input2 = new fuzzyVar("Humidity", ["Low", "Med", "High", "Very High"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.75,0.9],[0.8,1.0,1.2]]);
 var output1 = new fuzzyVar("Comfort", ["Uncomfortable", "Neutral", "Comfortable"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.8,1]]);
-var fuzzy = new fuzzySystem([input1, input2],[output1], []);
-fuzzy.processValue([0.39,0.65]);
+var rules = [["Temp", "Hot", "AND", "Humidity", "High", "THEN", "Comfort", "Uncomfortable"]];
+var fuzzy = new fuzzySystem([input1, input2],[output1], rules);
+fuzzy.processValue([0.90,0.85]);
 
