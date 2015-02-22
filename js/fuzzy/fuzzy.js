8fd4a48f702d21404b01217e475c920c0dd4c59c
DCode <dcode10@gmail.com>
2015-02-22 15:33:44 -0500

    Changed fuzzy inputs from arrays to dictionaries
    Changed internal representation of fuzzy inputs from arrays of the form
    [inputVar, setName, fuzzyValue], to dictionaries of the form
    fuzzyInputs[inputVar][setName]= fuzzyValue. The change makes for cleaner
    code and easier access to the desired value.

--- js/fuzzy/fuzzy.js
+++ js/fuzzy/fuzzy.js
@@ -17,17 +17,18 @@ function fuzzySystem(inputSets, outputSets, rules){
 
 fuzzySystem.prototype.processValue = function(value) {
 
-    var fuzzyValues = this.convertInputToFuzzy(value);
-    console.log(fuzzyValues);
+    var fuzzyInputs = this.convertInputToFuzzy(value);
+    console.log(fuzzyInputs);
+    //var fuzzyOutputs = this.applyRules(fuzzyInputs);
 };
 
 fuzzySystem.prototype.convertInputToFuzzy = function(value) {
 
-    var fuzzyInputs = [];
+    var fuzzyInputs = {};
 
     for(var inputNum = 0; inputNum < this.inputSets.length; inputNum++) {
+        fuzzyInputs[this.inputSets[inputNum].variableName] = {};
         var sets = this.inputSets[inputNum].setValues;
-        var setValues = {{}};
 
         for(var setNum = 0; setNum < sets.length; setNum++) {
             var currentSet = sets[setNum];
@@ -42,10 +43,16 @@ fuzzySystem.prototype.convertInputToFuzzy = function(value) {
             else {
                 fuzzyValue = 0;
             }
-            setValues = [this.inputSets[inputNum].variableName, this.inputSets[inputNum].setNames[setNum], fuzzyValue];
-            fuzzyInputs.push(setValues);
+
+            var inputVar = this.inputSets[inputNum].variableName;
+            var setName = this.inputSets[inputNum].setNames[setNum];
+            fuzzyInputs[inputVar][setName] = fuzzyValue;
         }
     }
 
     return fuzzyInputs;
 };
+
+fuzzySystem.prototype.applyRules = function(fuzzyInputs) {
+    return null;
+}
