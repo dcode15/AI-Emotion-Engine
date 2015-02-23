36dfd12514e283496719458a5c4d6ee7855316e1
DCode <dcode10@gmail.com>
2015-02-23 00:22:29 -0500

    Added fuzzy system initialization for event evaluation

--- js/engine/engine.js
+++ js/engine/engine.js
@@ -8,7 +8,6 @@ function Engine(goals){
 
 function Event(impact, importance, cause, associations) {
     this.impact = impact;
-    this.importance = importance;
     this.cause = cause;
     this.associations = items;
 }
@@ -18,6 +17,30 @@ Engine.prototype.triggerEvent = function(Event) {
     desirability = this.eventEval(Event);
 }
 
-Engine.prototype.eventEval(Event) {
+Engine.prototype.eventEval = function(Event) {
+    var impactVar = new FuzzyVar("Impact", ["HighlyNegative", "SlightlyNegative", "NoImpact", "SlightlyPositive", "HighlyPositive"],
+        [[-1.5,-1,-0.6], [-0.7,-0.4,-0.1], [-0.3,0,0.3], [0.1,0.4,0.7], [0.6,1,1.5]]);
+    var importanceVar = new FuzzyVar("Importance", ["NotImportant", "SlightlyImportant", "ExtremelyImportant"],
+        [[-0.5,0,0.4], [0.3,0.5,0.7], [0.6,1,1.5]]);
+    var desirabilityVar = new FuzzyVar("Desirability", ["HighlyUndesired", "SlightlyUndesired", "Neutral", "SlightlyDesired", "HighlyDesired"],
+        [[-1.5,-1,-0.6], [-0.7,-0.4,-0.1], [-0.3,0,0.3], [0.1,0.4,0.7], [0.6,1,1.5]]);
+    var rules = [["Impact","HighlyNegative","AND","Importance","NotImportant","THEN","Desirability","Neutral"],
+        ["Impact","SlightlyNegative","AND","Importance","NotImportant","THEN","Desirability","Neutral"],
+        ["Impact","NoImpact","AND","Importance","NotImportant","THEN","Desirability","Neutral"],
+        ["Impact","SlightlyPositive","AND","Importance","NotImportant","THEN","Desirability","Neutral"],
+        ["Impact","HighlyPositive","AND","Importance","NotImportant","THEN","Desirability","Neutral"],
+        ["Impact","HighlyNegative","AND","Importance","SlightlyImportant","THEN","Desirability","SlightlyUndesired"],
+        ["Impact","SlightlyNegative","AND","Importance","SlightlyImportant","THEN","Desirability","SlightlyUndesired"],
+        ["Impact","Neutral","AND","Importance","SlightlyImportant","THEN","Desirability","Neutral"],
+        ["Impact","SlightlyPositive","AND","Importance","SlightlyImportant","THEN","Desirability","SlightlyUndesired"],
+        ["Impact","HighlyPositive","AND","Importance","SlightlyImportant","THEN","Desirability","SlightlyUndesired"],
+        ["Impact","HighlyNegative","AND","Importance","ExtremelyImportant","THEN","Desirability","HighlyUndesired"],
+        ["Impact","SlightlyNegative","AND","Importance","ExtremelyImportant","THEN","Desirability","SlightlyUndesired"],
+        ["Impact","Neutral","AND","Importance","ExtremelyImportant","THEN","Desirability","Neutral"],
+        ["Impact","SlightlyPositive","AND","Importance","ExtremelyImportant","THEN","Desirability","SlightlyDesired"],
+        ["Impact","HighlyPositive","AND","Importance","ExtremelyImportant","THEN","Desirability","HighlyDesired"]];
 
+    var fuzzy = new FuzzySystem([impactVar, importanceVar],[desirabilityVar], rules);
+
+    //fuzzy.processValue([-0.65,0.65]);
 }
--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 /**
  * Created by Douglas on 2/18/2015.
  */
-var input1 = new FuzzyVar("Temp", ["Cold", "Warm", "Hot"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.8,1]]);
-var input2 = new FuzzyVar("Humidity", ["Low", "Med", "High", "Very High"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.75,0.9],[0.8,1.0,1.2]]);
-var output1 = new FuzzyVar("Comfort", ["Uncomfortable", "Neutral", "Comfortable"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.8,1]]);
-var rules = [["Temp", "Hot", "AND", "Humidity", "High", "THEN", "Comfort", "Uncomfortable"]];
-var fuzzy = new FuzzySystem([input1, input2],[output1], rules);
-fuzzy.processValue([0.90,0.85]);
+test = new Engine([]);
+test.eventEval();
 
