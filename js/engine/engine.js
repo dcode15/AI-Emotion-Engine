339868ef10a168b9c75507f5e3787a20bd24c4ff
DCode <dcode10@gmail.com>
2015-02-22 22:26:35 -0500

    Minor file system restructuring, began engine
    Created constructors for Engine and Event objects, and method stub for
    eventEval, the method that takes an event and assigns it a desirability
    value.

---
+++ js/engine/engine.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Douglas on 2/18/2015.
+ */
+
+function Engine(goals){
+    this.goals = goals;
+}
+
+function Event(impact, importance, cause, associations) {
+    this.impact = impact;
+    this.importance = importance;
+    this.cause = cause;
+    this.associations = items;
+}
+
+Engine.prototype.triggerEvent = function(Event) {
+
+    desirability = this.eventEval(Event);
+}
+
+Engine.prototype.eventEval(Event) {
+
+}
--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 /**
  * Created by Douglas on 2/18/2015.
  */
-var input1 = new fuzzyVar("Temp", ["Cold", "Warm", "Hot"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.8,1]]);
-var input2 = new fuzzyVar("Humidity", ["Low", "Med", "High", "Very High"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.75,0.9],[0.8,1.0,1.2]]);
-var output1 = new fuzzyVar("Comfort", ["Uncomfortable", "Neutral", "Comfortable"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.8,1]]);
+var input1 = new FuzzyVar("Temp", ["Cold", "Warm", "Hot"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.8,1]]);
+var input2 = new FuzzyVar("Humidity", ["Low", "Med", "High", "Very High"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.75,0.9],[0.8,1.0,1.2]]);
+var output1 = new FuzzyVar("Comfort", ["Uncomfortable", "Neutral", "Comfortable"], [[0,0.2,0.4], [0.3,0.5,0.7], [0.6,0.8,1]]);
 var rules = [["Temp", "Hot", "AND", "Humidity", "High", "THEN", "Comfort", "Uncomfortable"]];
-var fuzzy = new fuzzySystem([input1, input2],[output1], rules);
+var fuzzy = new FuzzySystem([input1, input2],[output1], rules);
 fuzzy.processValue([0.90,0.85]);
 
