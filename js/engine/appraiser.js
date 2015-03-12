dfa66024e787a22c1cfce387c1cc2073fca8125b
DCode <dcode10@gmail.com>
2015-03-12 18:26:57 -0400

    Fixed appraisal of Joy and Sadness

--- js/engine/appraiser.js
+++ js/engine/appraiser.js
@@ -6,10 +6,10 @@ Appraiser.prototype.appraiseEvent = function(event, standards, state) {
     var newEmotions = new Emotions();
 
     if(desirability > 0){
-        newEmotions.state["Joy"] = (1.7*Math.pow(expectations[event.name],0.5)) + (0.7*desirability);
+        newEmotions.state["Joy"] = (1.7*Math.pow(this.events[event.name]["Expectation"],0.5)) + (0.7*desirability);
     }
     if(desirability < 0){
-        newEmotions.state["Sad"] = (2*Math.pow(expectations[event.name],2))-desirability;
+        newEmotions.state["Sad"] = (2*Math.pow(this.events[event.name]["Expectation"],2))-desirability;
     }
 
 
--- js/engine/engine.js
+++ js/engine/engine.js
@@ -12,11 +12,6 @@ function Engine(id){
     this.appraiser = new Appraiser();
 }
 
-function Emotions() {
-    this.state = {"Joy":0,"Sad":0,"Disappointment":0,"Relief":0,"Hope":0,"Fear":0,"Pride":0,
-    "Shame":0,"Reproach":0,"Admiration":0,"Anger":0, "Gratitude":0,"Gratification":0,"Remorse":0};
-}
-
 Engine.prototype.addEvent = function(name, impacts,  expectation){
     this.events[name] = {};
     this.events[name]["Expectation"] = expectation;
@@ -40,4 +35,9 @@ Engine.prototype.addGoal = function(name, importance) {
 Engine.prototype.triggerEvent = function(name) {
     this.emotionalState = this.appraiser.appraiseEvent(name, this.standards, this.emotionalState);
     console.log(desirability);
+}
+
+function Emotions() {
+    this.state = {"Joy":0,"Sad":0,"Disappointment":0,"Relief":0,"Hope":0,"Fear":0,"Pride":0,
+        "Shame":0,"Reproach":0,"Admiration":0,"Anger":0, "Gratitude":0,"Gratification":0,"Remorse":0};
 }
